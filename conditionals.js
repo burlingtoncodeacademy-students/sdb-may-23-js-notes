0eafdbf7851d34b735cf5b9994987825ab82d71f
Paul Niemczyk <mrpndev@gmail.com>
2023-05-31 11:03:14 -0400

    Added ternaries and switch statements

--- conditionals.js
+++ conditionals.js
@@ -171,3 +171,106 @@ season == 1
     ! you MUST have an else statement
 
 */
+
+// Ternary Challenge Solution
+/* 
+    if age < 18 you're a teenager
+    if age >= 18 travel but no drinking
+    if age >= 21 you can drink
+*/
+
+let age = 28;
+
+// if (age >= 21) {
+// 	console.log("You can drink");
+// } else if (age >= 18) {
+// 	console.log("You can travel but cannot drink");
+// } else if (age < 18) {
+// 	console.log("You're a teenager");
+// }
+
+age >= 21
+	? console.log("You can drink")
+	: age >= 18
+	? console.log("You can travel but cannot drink")
+	: age < 18
+	? console.log("You're a teenager")
+	: null;
+
+// Leap Year Challenge Solution
+
+let year = 1900;
+// year/4 but not /100 OR
+// /400
+
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+	console.log(`The ${year} is a leap year`);
+} else {
+	console.log(`The ${year} is not a leap year`);
+}
+
+/* 
+    ? Switch Statements
+    * switches execute a block of code dependent upon a different case
+    * we ask question that requires a specific response determined by our answer
+*/
+
+let uprightInstructor = "Paul";
+
+switch (uprightInstructor) {
+	// information that cases are compared against
+	case "Paul":
+		// condition that's comapred against the switch
+		// executes the code below
+		console.log(`${uprightInstructor} is a Lead SD Instructor`);
+		break;
+	// used to stop other case from evaluating
+	case "Rob":
+		console.log(`${uprightInstructor} has dad jokes`);
+		break;
+	case "Henry":
+		console.log(`${uprightInstructor} is a Teaching Assistant`);
+		break;
+	default:
+		// equivalent of an else statement; a catch-all
+		console.log(`${uprightInstructor} may or may not work here.`);
+}
+
+/* 
+    ? Switch Challenge
+
+    * Create a grade variable that holds a value (ex: 78)
+
+    * Create a switch statement that checks against the variable.
+
+    * If it's above 89, console log "You got an A"
+    * greater than 79 B
+    * greater than 66 C
+    * greater than 59 D
+    * below 59 F
+    * default statement of "Input is not a grade"
+    
+    If you cannot create a switch, try an if/else or a ternary first
+*/
+
+grade = 62;
+
+switch (true) {
+	case grade >= 89:
+		console.log("A");
+		break;
+	case grade >= 79:
+		console.log("B");
+		break;
+	case grade >= 66:
+		console.log("C");
+		break;
+	case grade >= 59:
+		console.log("D");
+		break;
+	case grade < 59:
+		console.log("F");
+		break;
+	default:
+		console.log(`${grade} is not a grade`);
+}
